1fe73a1fb547cfd22176727e60e0b3cd9253046b
Alex Petrov <oleksandr.petrov@gmail.com>
2025-06-26 17:55:11 +0200

    Accord: Add Rebootstrap and unsafe Bootstrap To support recovering a node that has lost some of its local transaction log, introduce rebootstrap and unsafe bootstrap modes, where Accord ensures no responses are produced for transactions the node cannot be certain it had not previously answered.
    patch by Benedict; reviewed by Aleksey Yeschenko for CASSANDRA-20908

--- src/java/org/apache/cassandra/service/accord/AccordCommandStores.java
+++ src/java/org/apache/cassandra/service/accord/AccordCommandStores.java
@@ -37,9 +37,9 @@ import accord.utils.RandomSource;
 import org.apache.cassandra.cache.CacheSize;
 import org.apache.cassandra.config.AccordSpec.QueueShardModel;
 import org.apache.cassandra.config.DatabaseDescriptor;
-import org.apache.cassandra.schema.TableId;
 import org.apache.cassandra.service.accord.AccordExecutor.AccordExecutorFactory;
-import org.apache.cassandra.service.accord.api.TokenKey;
+import org.apache.cassandra.tcm.ClusterMetadata;
+import org.apache.cassandra.tcm.membership.NodeState;
 
 import static org.apache.cassandra.config.AccordSpec.QueueShardModel.THREAD_PER_SHARD;
 import static org.apache.cassandra.config.DatabaseDescriptor.getAccordQueueShardCount;
@@ -113,12 +113,12 @@ public class AccordCommandStores extends CommandStores implements CacheSize
     }
 
     @Override
-    protected boolean shouldBootstrap(Node node, Topology previous, Topology updated, Range range)
+    protected BootstrapRangeAction shouldBootstrap(Node node, Topology prevGlobal, Topology newLocal, Range range)
     {
-        if (!super.shouldBootstrap(node, previous, updated, range))
-            return false;
-        // we see new ranges when a new keyspace is added, so avoid bootstrap in these cases
-        return contains(previous, ((TokenKey)  range.start()).table());
+        if (NodeState.isBootstrap(ClusterMetadata.current().myNodeState()))
+            return BootstrapRangeAction.UNSAFE_BOOTSTRAP;
+
+        return super.shouldBootstrap(node, prevGlobal, newLocal, range);
     }
 
     @Override
@@ -126,17 +126,6 @@ public class AccordCommandStores extends CommandStores implements CacheSize
     {
         int idx = ((int) Thread.currentThread().getId()) & mask;
         return executors[idx].newSequentialExecutor();
-    }
-
-    private static boolean contains(Topology previous, TableId searchTable)
-    {
-        for (Range range : previous.ranges())
-        {
-            TableId table = ((TokenKey)  range.start()).table();
-            if (table.equals(searchTable))
-                return true;
-        }
-        return false;
     }
 
     public synchronized void setCapacity(long bytes)
